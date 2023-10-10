fc381e1f678c4e23340c8d7fba894436c7e4db0a
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-10-10 14:23:53 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.viswashbandhan',
+  appName: 'viswash-bandhan',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
